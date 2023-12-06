d4ccce0c1bdf56fe0522a5dfe104e721ec062af2
Mohammad GONS SAIB <mohagonssaib@gmail.com>
2023-12-06 21:24:27 +0100

    add style register form and fix issues

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -14,36 +14,119 @@ use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\IsTrue;
 use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Constraints\Email;
 
 class RegistrationFormType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email', EmailType::class)
+            ->add('email', EmailType::class, [
+                'label' => 'Email',
+                'attr' => [
+                    'class' => 'bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500',
+                    'placeholder' => 'name@company.com',
+                ],
+                'required' => true,
+                'constraints' => [
+                    new Email([
+                        'message' => 'L\'adresse email "{{ value }}" n\'est pas valide.',
+                    ]),
+                ],
+            ])
             ->add('plainPassword', PasswordType::class, [
+                'label' => 'Mot de passe',
                 'mapped' => false,
-                'attr' => ['autocomplete' => 'new-password'],
+                'attr' => [
+                    'class' => 'bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500',
+                    'autocomplete' => 'new-password',
+                    'placeholder' => 'Mot de passe',
+                ],
                 'constraints' => [
                     new NotBlank([
-                        'message' => 'Please enter a password',
+                        'message' => 'Veuillez saisir un mot de passe valide',
                     ]),
                     new Length([
                         'min' => 6,
-                        'minMessage' => 'Your password should be at least {{ limit }} characters',
+                        'minMessage' => 'Votre mot de passe doit contenir au moins {{ limit }} caractères',
                         'max' => 4096,
                     ]),
                 ],
             ])
-            ->add('firstname', TextType::class)
-            ->add('lastname', TextType::class)
-            ->add('address', TextType::class)
-            ->add('numberPhone', TelType::class)
+            ->add('firstname', TextType::class, [
+                'label' => 'Prénom',
+                'attr' => [
+                    'class' => 'bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500',
+                    'placeholder' => 'Prénom',
+                ],
+                'required' => true,
+                'constraints' => [
+                    new Length([
+                        'min' => 2,
+                        'minMessage' => 'Votre prénom doit contenir au moins {{ limit }} caractères',
+                        'max' => 30,
+                        'maxMessage' => 'Votre prénom doit contenir au maximum {{ limit }} caractères',
+                    ]),
+                ],
+            ])
+            ->add('lastname', TextType::class, [
+                'label' => 'Nom',
+                'attr' => [
+                    'class' => 'bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500',
+                    'placeholder' => 'Nom',
+                ],
+                'required' => true,
+                'constraints' => [
+                    new Length([
+                        'min' => 2,
+                        'minMessage' => 'Votre nom doit contenir au moins {{ limit }} caractères',
+                        'max' => 30,
+                        'maxMessage' => 'Votre nom doit contenir au maximum {{ limit }} caractères',
+                    ]),
+                ],
+            ])
+            ->add('address', TextType::class, [
+                'label' => 'Adresse',
+                'attr' => [
+                    'class' => 'bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500',
+                    'placeholder' => 'Adresse',
+                ],
+                'required' => true,
+                'constraints' => [
+                    new Length([
+                        'min' => 2,
+                        'minMessage' => 'Votre adresse doit contenir au moins {{ limit }} caractères',
+                        'max' => 30,
+                        'maxMessage' => 'Votre adresse doit contenir au maximum {{ limit }} caractères',
+                    ]),
+                ],
+            ])
+            ->add('numberPhone', TelType::class, [
+                'label' => 'Numéro de téléphone',
+                'attr' => [
+                    'class' => 'bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500',
+                    'placeholder' => 'Numéro de téléphone',
+                ],
+                'required' => true,
+                'constraints' => [
+                    new Length([
+                        'min' => 10,
+                        'minMessage' => 'Votre numéro de téléphone doit contenir au moins {{ limit }} caractères',
+                        'max' => 10,
+                        'maxMessage' => 'Votre numéro de téléphone doit contenir au maximum {{ limit }} caractères',
+                    ]),
+                ],
+            ])
             ->add('agreeTerms', CheckboxType::class, [
+                'label' => 'J\'accepte les conditions générales d\'utilisation',
                 'mapped' => false,
+                'required' => true,
+                'attr' => [
+                    'class' => 'w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800',
+                ],
                 'constraints' => [
                     new IsTrue([
-                        'message' => 'You should agree to our terms.',
+                        'message' => 'Tu devrais accepter nos termes d\'utilisation.',
                     ]),
                 ],
             ]);
