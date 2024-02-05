2a34f7d87de62bbacb37407c747399da4b9687dd
Théo <davigny.theo@gmail.com>
2024-02-05 10:40:04 +0100

    add homepage integration, modified base.html.twig and add 2 components

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,16 @@ module.exports = {
     "./templates/**/*.html.twig",
   ],
   theme: {
-    extend: {},
+    extend: {
+      height: {
+        '50vh': '50vh',
+      },
+      backgroundImage: {
+        'homepage': "url('../../public/images/3D_desktop_computer_computer_mouse-29771.jpg')",
+      },
+      backgroundColor: {
+        'footerBg': '#F8F9FA',
+      }
+    },
   },
 }
