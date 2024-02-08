5c92fdde604377effcae263383affeff7dfc27fc
Victor De Domenico <victordedomenico.du60@gmail.com>
2024-02-08 11:28:23 +0100

    MAJ Delete Form
    MAJ Delete Form

--- src/Controller/Company/CompanyController.php
+++ src/Controller/Company/CompanyController.php
@@ -9,12 +9,21 @@ use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Core\Security;
+use Symfony\Component\Security\Csrf\CsrfToken;
+use Symfony\Component\Security\Csrf\CsrfTokenManagerInterface;
 
 #[Route('/')]
 class CompanyController extends AbstractController
 {
+    private $security;
+
+    public function __construct(Security $security)
+    {
+        $this->security = $security;
+    }
+
     #[Route('/', name: 'app_company_index', methods: ['GET'])]
     public function index(UserRepository $userRepository): Response
     {
@@ -22,37 +31,7 @@ class CompanyController extends AbstractController
 
         $users = $userRepository->findBy(['company' => $company]);
 
-        return $this->render('company/index.html.twig', [
-            'users' => $users,
-            'company' => $company,
-        ]);
-    }
-
-    /*    #[Route('/new', name: 'app_company_new', methods: ['GET', 'POST'])]
-    public function new(Request $request, EntityManagerInterface $entityManager): Response
-    {
-        $company = new Company();
-        $form = $this->createForm(CompanyType::class, $company);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $entityManager->persist($company);
-            $entityManager->flush();
-
-            return $this->redirectToRoute('app_company_index', [], Response::HTTP_SEE_OTHER);
-        }
-
-        return $this->render('company/new.html.twig', [
-            'company' => $company,
-            'form' => $form,
-        ]);
-    }*/
-
-    private $security;
-
-    public function __construct(Security $security)
-    {
-        $this->security = $security;
+        return $this->render('company/index.html.twig', ['users' => $users, 'company' => $company,]);
     }
 
     #[Route('/new', name: 'app_company_new', methods: ['GET', 'POST'])]
@@ -73,18 +52,13 @@ class CompanyController extends AbstractController
             return $this->redirectToRoute('app_company_index');
         }
 
-        return $this->render('company/new.html.twig', [
-            'user' => $user,
-            'form' => $form->createView(),
-        ]);
+        return $this->render('company/new.html.twig', ['user' => $user, 'form' => $form->createView(),]);
     }
 
     #[Route('/{id}', name: 'app_company_show', methods: ['GET'])]
     public function show(User $user): Response
     {
-        return $this->render('company/show.html.twig', [
-            'user' => $user,
-        ]);
+        return $this->render('company/show.html.twig', ['user' => $user,]);
     }
 
     #[Route('/{id}/edit', name: 'app_company_edit', methods: ['GET', 'POST'])]
@@ -99,20 +73,23 @@ class CompanyController extends AbstractController
             return $this->redirectToRoute('app_company_index', [], Response::HTTP_SEE_OTHER);
         }
 
-        return $this->render('company/edit.html.twig', [
-            'user' => $user,
-            'form' => $form->createView(),
-        ]);
+        return $this->render('company/edit.html.twig', ['user' => $user, 'form' => $form->createView(),]);
     }
 
     #[Route('/{id}', name: 'app_company_delete', methods: ['POST'])]
-    public function delete(Request $request, User $user, EntityManagerInterface $entityManager): Response
+    public function delete(Request $request, User $user, EntityManagerInterface $entityManager, CsrfTokenManagerInterface $csrfTokenManager): Response
     {
-        if ($this->isCsrfTokenValid('delete'.$user->getId(), $request->request->get('_token'))) {
+        $token = new CsrfToken('delete' . $user->getId(), $request->request->get('_token'));
+        if ($csrfTokenManager->isTokenValid($token)) {
             $entityManager->remove($user);
             $entityManager->flush();
+
+            $this->addFlash('success', 'L\'utilisateur a été supprimé avec succès.');
+        } else {
+            $this->addFlash('error', 'Jeton de sécurité invalide, impossible de supprimer l\'utilisateur.');
         }
 
         return $this->redirectToRoute('app_company_index', [], Response::HTTP_SEE_OTHER);
     }
 }
+
