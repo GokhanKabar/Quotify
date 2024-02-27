42bb0f6ccba516000c941caa9a1782a01aad1173
GokhanKabar <gokhankabar@hotmail.fr>
2024-02-27 12:28:02 +0100

    Create front panel admin, redirect user when success login, fix footer responsive

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,9 +16,6 @@ module.exports = {
       height: {
         '50vh': '50vh',
       },
-      backgroundImage: {
-        'homepage': "url('../../public/images/3D_desktop_computer_computer_mouse-29771.jpg')",
-      },
       backgroundColor: {
         'footerBg': '#F8F9FA',
         'navbarBg': '#1F2937',
