46b2387f02dc8c54ac220699e1aca01a2923eec8
GokhanKabar <gokhankabar@hotmail.fr>
2024-03-02 18:54:57 +0100

    ajout de la balise de méta viewport dans base.html.twig fix responsive

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,9 +13,6 @@ module.exports = {
         'primary': '#1F2937',
         'secondary': '#836349',
       },
-      height: {
-        '50vh': '50vh',
-      },
       backgroundColor: {
         'footerBg': '#F8F9FA',
         'navbarBg': '#1F2937',
