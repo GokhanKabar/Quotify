39cd5fc1db767b8e2d89518307cc5c571ae96035
MohaGons <mohagonssaib@gmail.com>
2024-03-03 10:23:57 +0100

    clean code

--- migrations/Version20240303060846.php
+++ migrations/Version20240303060846.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20240302115204 extends AbstractMigration
+final class Version20240303060846 extends AbstractMigration
 {
     public function getDescription(): string
     {
